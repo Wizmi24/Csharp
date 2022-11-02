c24700870d9a1d1ef6287708f0e26b81d706b716
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2022-11-02 18:48:29 +0100

    Add files via upload

---
+++ WeekLater.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeekLater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Today is {DateTime.Now.ToString("d")} and week later is {DateTime.Now.AddDays(7).ToString("d")}");
+        }
+    }
+}
