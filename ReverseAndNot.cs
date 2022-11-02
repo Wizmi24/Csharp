6bf0dd86c1425ee1e7865146eb6f6037581c79c1
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2022-11-02 15:22:52 +0100

    Add files via upload

---
+++ ReverseAndNot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse
+{
+    internal class ReverseAndNot
+    {
+        static void Main(string[] args)
+        {
+            //Write a function that takes an integer i and returns a string with the integer backwards followed by the original integer.
+            Console.WriteLine("Enter integer: ");
+            var original = Console.ReadLine();
+            bool success = int.TryParse(original, out int result);
+            if (success&&result>=0)
+            {
+                Console.WriteLine(string.Concat(original.Reverse())+original);             
+            }
+            else
+            {
+                Console.WriteLine("You entered wrong data");
+            }
+        }
+    }
+}
