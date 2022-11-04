079aea6abcad756c84395f75c6b561c651518930
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2022-11-04 16:50:49 +0100

    Add files via upload

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Fibonacci
+    {
+        public static string zadanie(int number)
+        {
+            var fibonacciSequence = new List<string> { "b", "a"};
+
+            if (number < 3)
+                return "invalid";
+            
+            for(int i = 2; i < number; i++)
+            {
+                fibonacciSequence.Add(fibonacciSequence[i - 1] + fibonacciSequence[i-2]);
+            }
+            return string.Join(",", fibonacciSequence);
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                A Fibonacci Word is a specific sequence of binary digits (or symbols from any two-letter alphabet). The Fibonacci Word is formed by repeated concatenation in the same way that the Fibonacci numbers are formed by repeated addition.
+
+                Create a function that takes a number n as an argument and returns the first n elements of the Fibonacci Word sequence.
+
+                If n < 2, the function must return "invalid".
+             */
+
+            Console.WriteLine("Enter number of fibonacci characters");
+            var number = Console.ReadLine();
+            bool success=int.TryParse(number, out int result);
+            if (success)
+            {
+                Console.WriteLine(Fibonacci.zadanie(result));
+            }
+            else
+            {
+                Console.WriteLine("You have to enter a number");
+            }
+        }
+    }
+}
