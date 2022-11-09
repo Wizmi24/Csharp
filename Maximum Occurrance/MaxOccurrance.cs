b024e81d0d29b3c211122311882ecaa96b1c2468
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2022-11-09 21:03:57 +0100

    Add files via upload

---
+++ Maximum Occurrance/MaxOccurrance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Occurrance
+{
+    internal class MaxOccurrance
+    {
+        public static string zadanie(string userInput)
+        {
+            var groups = userInput.ToCharArray().OrderBy(x => x).GroupBy(x => x);
+            var max = groups.Max(x => x.Count());
+            return max > 1 ? "\""+String.Join("\", \"", groups.Where(x => x.Count() == max).Select(x=>x.Key.ToString()).ToArray())+"\"":"No repetition";
+
+        }
+    }
+}
---
+++ Maximum Occurrance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Maximum_Occurrance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Given a string text. Write a function that returns the character with the highest frequency.
+             If more than 1 character has the same highest frequency, return all those characters as a comma separated string.
+             If there is no repetition in characters, return "No Repetition".
+             */
+
+            Console.WriteLine("Enter string");
+            var userInput=Console.ReadLine();
+            if (string.IsNullOrEmpty(userInput))
+                throw new Exception("You cannot enter empty string");
+
+            
+            Console.WriteLine("The most common characters in entered sentence : "+MaxOccurrance.zadanie(userInput));
+
+        }
+    }
+}
