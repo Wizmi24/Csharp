214e5c150e05208b65d26f566430911621672c56
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2022-11-10 20:54:04 +0100

    Add files via upload

---
+++ String Incrementer/IncresmentString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Incrementer
+{
+    internal class IncresmentString
+    {
+        public static string zadanie(string userInput)
+        {
+            List<int> list = new List<int>();
+
+            foreach (var character in userInput)
+            {
+                if(int.TryParse(character.ToString(), out int result))
+                {
+                    list.Add(result);
+                }
+                else if(list.Count > 0)
+                    list.Clear();
+            }
+
+            string shortString="";
+            int number=0;
+
+
+            if (list.Count!=0)
+            {
+                shortString = new string(userInput.ToArray(), 0, userInput.Length - list.Count);
+                string numbers = string.Join("", list);
+                number= int.Parse(numbers);
+                number++;
+            }
+
+
+            if (list.Count == 0)
+                return userInput + "1";
+            else
+                return shortString + number.ToString();
+        }
+    }
+}
---
+++ String Incrementer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace String_Incrementer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Write a function which increments a string to create a new string.
+
+             If the string ends with a number, the number should be incremented by 1.
+             If the string doesn't end with a number, 1 should be added to the new string.
+             If the number has leading zeros, the amount of digits should be considered.
+             */
+            Console.WriteLine("Please enter a string");
+            var userInput = Console.ReadLine();
+            if (string.IsNullOrEmpty(userInput))
+                throw new Exception("You must enter something");
+
+            Console.WriteLine(IncresmentString.zadanie(userInput));
+        }
+    }
+}
