f0e4a458938f2ca0d1f2de5e15bc0fbc389099a1
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2022-11-14 21:30:37 +0100

    Add files via upload

---
+++ Fruit Smoothie/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fruit_Smoothie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Create a class Smoothie and do the following:
+             Create a property called Ingredients.
+             Create a GetCost method which calculates the total cost of the ingredients used to make the smoothie.
+             Create a GetPrice method which returns the number from GetCost plus the number from GetCost multiplied by 1.5. Round to two decimal places.
+             Create a GetName method which gets the ingredients and puts them in alphabetical order into a nice descriptive sentence.
+             If there are multiple ingredients, add the word "Fusion" to the end but otherwise, add "Smoothie".
+             Remember to change "-berries" to "-berry".
+             */
+
+            var s1 = new Smoothie(new string[] { "Banana", "Pineapple", "Apple" });
+            Console.WriteLine("Get Name: " + s1.GetName());
+            Console.WriteLine("Get Cost: " + s1.GetCost());
+            Console.WriteLine("Get Price: " + s1.GetPrice());
+            var s2 = new Smoothie(new string[] { "Raspberries", "Strawberries", "Blueberries" });
+            Console.WriteLine("Get Name: " + s2.GetName());
+            Console.WriteLine("Get Cost: " + s2.GetCost());
+            Console.WriteLine("Get Price: " + s2.GetPrice());
+            var s3 = new Smoothie(new string[] { "Banana" });
+            Console.WriteLine("Get Name: " + s3.GetName());
+            Console.WriteLine("Get Cost: " + s3.GetCost());
+            Console.WriteLine("Get Price: " + s3.GetPrice());
+        }
+    }
+}
---
+++ Fruit Smoothie/Smoothie.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruit_Smoothie
+{
+    internal class Smoothie
+    {
+        private static Dictionary<string, decimal> prices = new Dictionary<string, decimal>()
+        {
+            {"Strawberries", 1.50m },
+            {"Banana", 0.50m },
+            {"Mango", 2.50m },
+            {"Blueberries", 1m },
+            {"Raspberries", 1m },
+            {"Apple", 1.75m },
+            {"Pineaple", 3.50m }
+        };
+
+        public static string[] Ingredients { get; set; }
+
+        public Smoothie(IEnumerable<string> ingredients)
+        {
+            Ingredients= ingredients.ToArray();
+        }
+
+        
+        public decimal GetCost() {
+            decimal cost = 0;
+            foreach (var item in Ingredients)
+            {
+                decimal price = 0;
+                prices.TryGetValue(item, out price);
+                cost+=price;
+            }
+            return cost;
+        }
+        public decimal GetPrice() {
+            return Math.Round((GetCost() * (decimal)2.5), 2);
+        }
+        public string GetName() {
+            return String.Join(" ", Ingredients.OrderBy(x=>x).Select(y=>y.EndsWith("ies")?y.Substring(0, y.Length-3)+"y":y))+(Ingredients.Length>1?" Fusion":" Smoothie");
+        }
+    }
+}
