35b377a54bb792b17013520f41c37b6598bb8716
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-01-05 18:41:08 +0100

    Add files via upload

---
+++ DeadEndNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ This number sequence can start with any positive integer n.
+ s is the sum of the individual digits in n.
+ If s divides into n evenly then the next term of the series is n//s.
+ Otherwise the next term is n*s. Eventually the series will reach a dead end with two numbers alternating: 58, 754, 12064, 928, 17632, 928, 17632.
+ This series has a length of 5 with 17632 as the last term.
+
+Compose a function that takes a positive integer and returns its series length and its last term.
+ */
+
+using DeadEndNumber;
+
+string userInput;
+bool flag;
+int number;
+
+Console.WriteLine("Enter positive integer");
+do
+{
+    userInput = Console.ReadLine();
+    flag = Int32.TryParse(userInput, out number);
+} while (!flag||number<1);
+
+var seq = new Sequence();
+seq.SequenceLenght(number);
---
+++ DeadEndNumber/Sequence.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadEndNumber
+{
+    public class Sequence
+    {
+        private int _sum { get; set; } = 0;
+        private int _count { get; set; } = 0;
+        private int _previousSequence { get; set; }
+        private int _numberSequence { get; set; }
+        private int _temp { get; set; }
+        private string _numberString { get; set; }
+        public void SequenceLenght(int number)
+        {
+            _previousSequence = number;
+            _numberSequence=number;
+            do
+            {
+                if (_numberSequence == _previousSequence&&_count>0)
+                {
+                    Console.WriteLine($"The sequence is {_count} long and the last number is {_numberSequence}");
+                    return;
+                }
+
+                if (_count > 1)
+                {
+                    _previousSequence = _temp;
+                }
+
+                _temp = _numberSequence;
+                _count++;
+                _sum = 0;
+
+                if (_numberSequence < 10)
+                {
+                    _sum = _numberSequence;
+                }
+                else
+                {
+                    _numberString = _numberSequence.ToString();
+                    for(int i = 0; i < _numberString.Length; i++)
+                    {
+                        _sum += Int32.Parse(_numberString.Substring(i, 1));
+                    }
+                }
+
+                if (_numberSequence % _sum == 0)
+                {
+                    _numberSequence /= _sum;
+                }
+                else
+                {
+                    _numberSequence *= _sum;
+                }
+
+            } while (true);
+
+        }
+    }
+}
