d35ebdefc7d15b5f87ec350b8e78bcabef68616a
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-03-27 22:02:21 +0200

    Add files via upload

---
+++ SortingASCaDESC/Program.cs
@@ -0,0 +1,26 @@
+﻿using SortingASCaDESC;
+
+var sort = new Sort();
+
+var answear = sort.SortContacts(new string[] {
+  "John Locke",
+  "Thomas Aquinas",
+  "David Hume",
+  "Rene Descartes"
+}, "ASC");
+Console.WriteLine("Ascending");
+foreach (var item in answear)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+answear = sort.SortContacts(new string[] {
+  "Paul Erdos",
+  "Leonhard Euler",
+  "Carl Gauss"
+}, "DESC");
+Console.WriteLine("Descending");
+foreach (var item in answear)
+{
+    Console.WriteLine(item);
+}
---
+++ SortingASCaDESC/Sort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingASCaDESC
+{
+    public class Sort
+    {
+        public string[] SortContacts(string[] contacts, string option)
+        {
+            Func<string, string> getLastName = name => name.Split(" ")[1];
+            if(option == "ASC")
+            {
+                return contacts.OrderBy(getLastName).ToArray();
+            }
+            else
+            {
+                return contacts.OrderByDescending(getLastName).ToArray();
+            }
+        }
+    }
+}
