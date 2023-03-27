337961361949187487738d6e71db949f0cc7022f
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-03-27 23:45:05 +0200

    Add files via upload

---
+++ TrackTheRobot/Program.cs
@@ -0,0 +1,16 @@
+﻿using TrackTheRobot;
+
+var robot = new Tracking();
+
+var position = robot.TrackRobot("up 40", "down 10", "right 30", "left 20");
+Console.WriteLine("Horizontally: "+position[0,0]+ " Vertically: " + position[1,0]);
+
+var robot2 = new Tracking();
+
+position = robot2.TrackRobot("up 10", "down 30", "right 20", "left 70");
+Console.WriteLine("Horizontally: " + position[0, 0] + " Vertically: " + position[1, 0]);
+
+var robot3 = new Tracking();
+
+position = robot3.TrackRobot("up 40", "down 40", "right 30", "left 30");
+Console.WriteLine("Horizontally: " + position[0, 0] + " Vertically: " + position[1, 0]);
---
+++ TrackTheRobot/Tracking.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackTheRobot
+{
+    public class Tracking
+    {
+        private int[,] Position = { {0 },{0 } };
+
+        public int[,] TrackRobot(params string[] instructions)
+        {
+            Func<string, string> getDirection = direction=> direction.Split(" ")[0].ToLowerInvariant();
+            Func<string, string> getValue = value => value.Split(" ")[1];
+            foreach (var item in instructions)
+            {
+                if(getDirection(item) == "right")
+                {
+                    Position[0,0] +=Convert.ToInt32(getValue(item));
+                }
+                if (getDirection(item) == "left")
+                {
+                    Position[0, 0] -= Convert.ToInt32(getValue(item));
+                }
+                if (getDirection(item) == "up")
+                {
+                    Position[1, 0] += Convert.ToInt32(getValue(item));
+                }
+                if (getDirection(item) == "down")
+                {
+                    Position[1, 0] -= Convert.ToInt32(getValue(item));
+                }
+            }
+            return Position;
+        }
+    }
+}
