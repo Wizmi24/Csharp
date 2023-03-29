f1b8187f4a0e4cbc3f6ba69de6dbe03fc33d2bcd
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-03-29 18:00:52 +0200

    Add files via upload

---
+++ BigNumArray/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using BigNumArray;
+
+var p1 = new SearchForBiggestArrayNumber();
+var answear = p1.FindLargest(new double[][] { new double[] { 4, 2, 7, 1 }, new double[] { 20, 70, 40, 90 }, new double[] { 1, 2, 0 } });
+foreach (var item in answear)
+{
+    Console.WriteLine(item);
+}
---
+++ BigNumArray/SearchForBiggestArrayNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigNumArray
+{
+    public class SearchForBiggestArrayNumber
+    {
+        public double[] FindLargest(double[][] numbers)
+        {
+            double[] result = new double[numbers.Length];
+            var i = 0;
+            foreach (var item in numbers)
+            {
+                result[i++] = item.Max();
+            }
+            return result;
+        }
+    }
+}
