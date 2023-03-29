bd3e8aaba7efbd75c1be19a9b3c03c25bae70c16
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-03-29 17:39:29 +0200

    Add files via upload

---
+++ UncensoreString/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using UncensoreString;
+
+var p1 = new Uncensoring();
+
+Console.WriteLine(p1.Uncensore("Wh*r* d*d my v*w*ls g*?", "eeioeo"));
+Console.WriteLine(p1.Uncensore2("*PP*RC*S*", "UEAE"));
---
+++ UncensoreString/Uncensoring.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UncensoreString
+{
+    public class Uncensoring
+    {
+        public string Uncensore(string sentence, string vowels)
+        {
+            char[] vow = vowels.ToCharArray();
+            char[] sen = sentence.ToCharArray();
+
+            for (int i = 0, j=0;i < sen.Length;i++)
+            {
+                if(sen[i] == '*')
+                {
+                    sen[i] = vow[j];
+                    j++;
+                }
+            }
+
+            return new string(sen);
+        }
+
+        public string Uncensore2(string sentence, string vowels)
+        {
+            var q = new Queue<char>(vowels.ToCharArray());
+            return Regex.Replace(sentence, @"\*", m => q.Dequeue().ToString());
+        }
+    }
+}
