9d5545c57d71409716ed1418ec855cfd47634e48
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-03-30 16:15:10 +0200

    Add files via upload

---
+++ ConvertArrToStr/Converter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertArrToStr
+{
+    public class Converter
+    {
+        public string[] ConvertIt(object[] arr)
+        {
+            return Array.ConvertAll(arr, x => x.ToString());
+        }
+    }
+}
---
+++ ConvertArrToStr/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConvertArrToStr;
+
+var p1 = new Converter();
+
+Console.WriteLine(string.Join(",", p1.ConvertIt(new object[] { 1, 2, "a", "b" })));
+Console.WriteLine(string.Join(",", p1.ConvertIt(new object[] { "a", "b", 10, 115 })));
+Console.WriteLine(string.Join(",", p1.ConvertIt(new object[] { })));
+Console.WriteLine(string.Join(",", p1.ConvertIt(new object[] { -4, "k", 0 })));
+Console.WriteLine(string.Join(",", p1.ConvertIt(new object[] { "Hell000!", 5 })));
