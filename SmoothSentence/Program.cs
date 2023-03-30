fe13b8bb43022c591248f485204333bc8e058d8e
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-03-30 16:50:30 +0200

    Add files via upload

---
+++ SmoothSentence/Program.cs
@@ -0,0 +1,6 @@
+﻿using SmoothSentence;
+
+var p1 = new Smooth();
+
+Console.WriteLine("Is \"Marta appreciated deep perpendicular right trapezoids\" smooth sentence? " +  p1.IsSmooth("Marta appreciated deep perpendicular right trapezoids"));
+Console.WriteLine("Is \"Someone is outside the doorway\" smooth sentence? " + p1.IsSmooth("Someone is outside the doorway"));
---
+++ SmoothSentence/Smooth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothSentence
+{
+    public class Smooth
+    {
+        public bool IsSmooth(string sentence)
+        {
+            var words = sentence.Split(' ');
+            for (int i = 0; i < words.Length-1; i++)
+            {
+                if (!words[i].EndsWith(words[i + 1][0]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
