4bda6c55fc2dc6f998519cc05ac8d8cb246f37f6
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-03-30 15:14:40 +0200

    Add files via upload

---
+++ TimeLimit/Program.cs
@@ -0,0 +1,13 @@
+﻿using TimeLimit;
+
+var p1 = new Test();
+
+Console.WriteLine("Candidate is " + p1.Limit(new int[] { 5, 5, 10, 10, 15, 15, 20, 20 }, 120));
+Console.WriteLine();
+Console.WriteLine("Candidate is " + p1.Limit(new int[] { 2, 3, 8, 6, 5, 12, 10, 18 }, 64));
+Console.WriteLine();
+Console.WriteLine("Candidate is " + p1.Limit(new int[] { 5, 5, 10, 10, 25, 15, 20, 20 }, 120));
+Console.WriteLine();
+Console.WriteLine("Candidate is " + p1.Limit(new int[] { 5, 5, 10, 10, 15, 15, 20 }, 120));
+Console.WriteLine();
+Console.WriteLine("Candidate is " + p1.Limit(new int[] { 5, 5, 10, 10, 15, 15, 20, 20 }, 130));
---
+++ TimeLimit/Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeLimit
+{
+    public class Test
+    {
+        public string Limit(int[] timer, int overallTime)
+        {
+            if (timer.Length < 8||overallTime>120)
+                return "disqualified";
+
+            var timeLimit = 0;
+
+            for(int i = 1; i < timer.Length; i++)
+            {
+                if (i%2==1)
+                {
+                    timeLimit += 5;
+                }
+
+                Console.WriteLine("Time Limit " + timeLimit);
+                Console.WriteLine("Candidate Time " + timer[i-1]);
+                Console.WriteLine();
+
+                if(timer[i-1] > timeLimit)
+                {
+                    return "disqualified";
+                }
+            }
+
+            return "qualified";
+        }
+    }
+}
