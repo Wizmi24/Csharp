226d1d173b46151dcf6ee25a2548444b38737187
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-05 15:35:15 +0200

    Add files via upload

---
+++ ConsecitiveNumbers/ConsNum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsecitiveNumbers
+{
+    public class ConsNum
+    {
+        public bool Cons(int[] arr)
+        {
+            if (arr.Distinct().Count()!=arr.Length)
+            {
+                return false;
+            }
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                if (arr[i]+1 != arr[i+1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ConsecitiveNumbers/Program.cs
@@ -0,0 +1,7 @@
+﻿using ConsecitiveNumbers;
+
+var p1 = new ConsNum();
+
+var answear = p1.Cons(new int[] { 1, 2, 3 , 7, 4});
+
+Console.WriteLine("Are the numbers consecutive? " + answear);
