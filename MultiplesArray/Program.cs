79918acb014d624bb5aca667ed5f2e55bacdc98d
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-05 18:32:25 +0200

    Add files via upload

---
+++ MultiplesArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using MultiplesArray;
+
+var p1 = new ReturningArray();
+
+var ans = p1.ArrayOfMultiples(7,5);
+Console.WriteLine(string.Join(",", ans));
+
+ans = p1.ArrayOfMultiples(12, 10);
+Console.WriteLine(string.Join(",", ans));
+
+ans = p1.ArrayOfMultiples(17, 7);
+Console.WriteLine(string.Join(",", ans));
+
+ans = p1.ArrayOfMultiples(630, 14);
+Console.WriteLine(string.Join(",", ans));
+
+ans = p1.ArrayOfMultiples(140, 3);
+Console.WriteLine(string.Join(",", ans));
---
+++ MultiplesArray/ReturningArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesArray
+{
+    public class ReturningArray
+    {
+        public int[] ArrayOfMultiples(int num, int length)
+        {
+            int[] array = new int[length];
+            for (int i = 1; i <= length; i++)
+            {
+                array[i-1] = num*i;
+            }
+            return array;
+        }
+    }
+}
