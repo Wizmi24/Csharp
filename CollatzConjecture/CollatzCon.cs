3c15f7a18027f90edebdd2fdd75399ab331220e4
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-05 16:45:39 +0200

    Add files via upload

---
+++ CollatzConjecture/CollatzCon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollatzConjecture
+{
+    public class CollatzCon
+    {
+        public int Col(int num)
+        {
+            var steps = 0;
+
+            while (num != 1)
+            {
+                if(num% 2 == 0)
+                {
+                    num /= 2;
+                    steps++;
+                }
+                else
+                {
+                    num = num * 3 + 1;
+                    steps++;
+                }
+            }
+            return steps;
+        }
+    }
+}
---
+++ CollatzConjecture/Program.cs
@@ -0,0 +1,9 @@
+﻿using CollatzConjecture;
+
+var p1 = new CollatzCon();
+
+Console.WriteLine($"It took {p1.Col(2)} steps to reach 1");
+Console.WriteLine($"It took {p1.Col(3)} steps to reach 1");
+Console.WriteLine($"It took {p1.Col(10)} steps to reach 1");
+Console.WriteLine($"It took {p1.Col(6)} steps to reach 1");
+Console.WriteLine($"It took {p1.Col(345)} steps to reach 1");
