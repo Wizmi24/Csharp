aa5e54c84e58276d31f03b08a3aea2d9fa83e2a8
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-05 18:07:00 +0200

    Add files via upload

---
+++ ReturningMiddleCharacter/Program.cs
@@ -0,0 +1,15 @@
+﻿using ReturningMiddleCharacter;
+
+var p1 = new ReturnCharacter();
+
+var ans = p1.MiddleChar("test");
+Console.WriteLine(ans);
+
+ans = p1.MiddleChar("testing");
+Console.WriteLine(ans);
+
+ans = p1.MiddleChar("middle");
+Console.WriteLine(ans);
+
+ans = p1.MiddleChar("A");
+Console.WriteLine(ans);
---
+++ ReturningMiddleCharacter/ReturnCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturningMiddleCharacter
+{
+    public class ReturnCharacter
+    {
+        public string MiddleChar(string str)
+        {
+            return (str.Length % 2 == 0) ? str.Substring(str.Length / 2 - 1, 2) : str.Substring(str.Length / 2, 1);
+        }
+    }
+}
