08864831ce51dbbd737d8fa60b2e9bbe9a41468f
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-05 16:07:39 +0200

    Add files via upload

---
+++ EqualityCheck/CheckingEquality.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualityCheck
+{
+    public class CheckingEquality
+    {
+        public bool isEqual(object a, object b)
+        {
+            return a.Equals(b);
+        }
+    }
+}
---
+++ EqualityCheck/Program.cs
@@ -0,0 +1,9 @@
+﻿using EqualityCheck;
+
+var p1 = new CheckingEquality();
+
+Console.WriteLine("Is it equal? " + p1.isEqual("a","b"));
+Console.WriteLine("Is it equal? " + p1.isEqual("1", 1));
+Console.WriteLine("Is it equal? " + p1.isEqual(1, true));
+Console.WriteLine("Is it equal? " + p1.isEqual("a", 'a'));
+Console.WriteLine("Is it equal? " + p1.isEqual("1", "1"));
