77dbd7f9338fecbd04fe050d0803ed5e62c0ce73
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-05 15:52:31 +0200

    Add files via upload

---
+++ CounterpartCharCode/CounterPartChar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterpartCharCode
+{
+    public class CounterPartChar
+    {
+        public int CharacterChar(char symbol)
+        {
+            return ((int)symbol)>90?(int)symbol-32:(int)symbol+32;
+        }
+
+        public int CharacterChar2(char symbol)
+        {
+            if (Char.IsUpper(symbol))
+            {
+                return (int)char.ToLower(symbol);
+            }
+            else
+            {
+                return (int)char.ToUpper(symbol);
+            }
+                
+        }
+    }
+}
---
+++ CounterpartCharCode/Program.cs
@@ -0,0 +1,5 @@
+﻿using CounterpartCharCode;
+
+var p1 = new CounterPartChar();
+
+Console.WriteLine("Counterpart char code of a is: " +  p1.CharacterChar2('a'));
