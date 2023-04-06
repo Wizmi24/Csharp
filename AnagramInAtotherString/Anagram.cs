01304f7485bc6632fc3cf2a664899fad18577080
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-06 21:45:26 +0200

    Add files via upload

---
+++ AnagramInAtotherString/Anagram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace AnagramInAtotherString
+{
+    public class Anagram
+    {
+        public bool AnagramSearch(string needle, string haystack)
+        {
+            char[] needleChars = needle.ToCharArray();
+            var tempChars = new List<char>(needleChars);
+            bool flag = true;
+            for (var i = 0; i < haystack.Length ; i++)
+            {
+                if (flag==false)
+                {
+                    tempChars = new List<char>(needleChars);
+                }
+
+                flag = false;
+
+                for (int j = 0; j < tempChars.Count; j++)
+                {
+                    if (haystack[i] == tempChars[j])
+                    {
+                        tempChars.Remove(tempChars[j]);
+                        flag = true;
+                        if(tempChars.Count == 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ AnagramInAtotherString/Program.cs
@@ -0,0 +1,11 @@
+﻿using AnagramInAtotherString;
+
+var p1 = new Anagram();
+
+Console.WriteLine("Result: " + p1.AnagramSearch("car", "race"));
+Console.WriteLine();
+Console.WriteLine("Result: " + p1.AnagramSearch("nod", "done"));
+Console.WriteLine();
+Console.WriteLine("Result: " + p1.AnagramSearch("bag", "grab"));
+Console.WriteLine();
+Console.WriteLine("Result: " + p1.AnagramSearch("caree", "race"));
