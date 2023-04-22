1c5ee25c4119cd17c00016ec0cbe2ab8c555fdf1
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-22 14:46:20 +0200

    Add files via upload

---
+++ PositiveCountNegativeSum/ArrayMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositiveCountNegativeSum
+{
+    public class ArrayMath
+    {
+        public int[] CountPosSumNeg(int[] arr)
+        {
+            int count = 0, sum = 0;
+            foreach (var item in arr)
+            {
+                if(item > 0)
+                {
+                    count++;
+                }
+                else
+                {
+                    sum += item;
+                }
+            }
+
+            return new[] { count, sum };
+        }
+    }
+}
---
+++ PositiveCountNegativeSum/Program.cs
@@ -0,0 +1,7 @@
+﻿using PositiveCountNegativeSum;
+
+var p1 = new ArrayMath();
+
+var result = p1.CountPosSumNeg(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 });
+
+Console.WriteLine($"Array count: {result[0]}, Array Sum: {result[1]}");
