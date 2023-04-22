b1743f68f18e783ad90a910cccad4d987343b84b
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-22 16:33:10 +0200

    Add files via upload

---
+++ HighestLowest/Program.cs
@@ -0,0 +1,5 @@
+﻿using HighestLowest;
+
+var p1 = new StringMath();
+
+Console.WriteLine("Highest and Lowest numbers: "+p1.HighLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
---
+++ HighestLowest/StringMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighestLowest
+{
+    public class StringMath
+    {
+        public string HighLow(string str)
+        {
+            var split = str.Split(' ').Select(int.Parse);
+
+            return new string(split.Max()+" "+split.Min());
+        }
+    }
+}
