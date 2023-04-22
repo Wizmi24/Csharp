afabcbde21fca591d9ab1944b3200dc64bdc93f7
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-22 14:30:06 +0200

    Add files via upload

---
+++ RegexValidHex/Program.cs
@@ -0,0 +1,10 @@
+﻿using RegexValidHex;
+
+var p1 = new ValidHex();
+
+Console.WriteLine("This is valid hex: " + p1.RegexMethod("#CD5C5C"));
+Console.WriteLine("This is valid hex: " + p1.RegexMethod("#EAECEE"));
+Console.WriteLine("This is valid hex: " + p1.RegexMethod("#eaecee"));
+Console.WriteLine("This is valid hex: " + p1.RegexMethod("#CD5C58C"));
+Console.WriteLine("This is valid hex: " + p1.RegexMethod("#CD5C5Z"));
+Console.WriteLine("This is valid hex: " + p1.RegexMethod("#CD5C&C"));
