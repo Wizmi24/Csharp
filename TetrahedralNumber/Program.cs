9fd88d5ed97edd6d50127bb44ea694cf91e122bb
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-24 22:53:57 +0200

    Add files via upload

---
+++ TetrahedralNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿using TetrahedralNumber;
+
+var p1 = new Tetrahedral();
+
+Console.WriteLine("Number of items: " + p1.NthNumber(1));
+Console.WriteLine("Number of items: " + p1.NthNumber(2));
+Console.WriteLine("Number of items: " + p1.NthNumber(3));
+Console.WriteLine("Number of items: " + p1.NthNumber(4));
---
+++ TetrahedralNumber/Tetrahedral.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrahedralNumber
+{
+    public class Tetrahedral
+    {
+        public int NthNumber(int n)
+        {
+            return (n * (n + 1) * (n + 2)) / 6;
+        }
+    }
+}
