c929478c5f3668665a5a3c15471a2d5ab800d935
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-24 20:59:43 +0200

    Add files via upload

---
+++ NextPrime/Prime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextPrime
+{
+    public class Prime
+    {
+        public int NextPrime(int num)
+        {
+            while (true)
+            {
+                if (IsPrime(num))
+                {
+                    return num;
+                }
+                num++;
+            }
+        }
+
+        private bool IsPrime(int num)
+        {
+            for(int i = 2; i < num; i++)
+            {
+                if(num% i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ NextPrime/Program.cs
@@ -0,0 +1,10 @@
+﻿using NextPrime;
+
+var p1 = new Prime();
+
+//Given an integer, create a function that returns the next prime. If the number is prime, return the number itself.
+
+Console.WriteLine("Next prime is: " + p1.NextPrime(12));
+Console.WriteLine("Next prime is: " + p1.NextPrime(24));
+Console.WriteLine("Next prime is: " + p1.NextPrime(11));
+Console.WriteLine("Next prime is: " + p1.NextPrime(13));
