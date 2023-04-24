409dd27fa7227e1991a2e20a4b257d4d4646c680
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-24 22:31:06 +0200

    Add files via upload

---
+++ VertexOfQuadratic/Program.cs
@@ -0,0 +1,12 @@
+﻿using VertexOfQuadratic;
+
+var p1 = new QuadraticCurve();
+
+var answear = p1.Vertex(-1, 0, 25);
+Console.WriteLine($"X: {answear[0]}, Y: {answear[1]}");
+
+answear = p1.Vertex(1, 10, 25);
+Console.WriteLine($"X: {answear[0]}, Y: {answear[1]}");
+
+answear = p1.Vertex(8, 4, 0);
+Console.WriteLine($"X: {answear[0]}, Y: {answear[1]}");
---
+++ VertexOfQuadratic/QuadraticCurve.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VertexOfQuadratic
+{
+    public class QuadraticCurve
+    {
+        public double[] Vertex(int a, int b, int c)
+        {
+            double x = -b/(double)(2*a);
+            double y = a * x * x + b * x + c;
+            return new double[] { x, y };
+        }
+    }
+}
