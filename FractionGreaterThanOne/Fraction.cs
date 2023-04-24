a3aba7e985650b0d9df36a5c7cda095888f291cc
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-04-24 22:44:49 +0200

    Add files via upload

---
+++ FractionGreaterThanOne/Fraction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionGreaterThanOne
+{
+    public class Fraction
+    {
+        public bool IsGreaterThanOne(string str)
+        {
+            var split = str.Split("/").Select(int.Parse).ToArray();
+            return split[0] > split[1];
+        }
+    }
+}
---
+++ FractionGreaterThanOne/Program.cs
@@ -0,0 +1,6 @@
+﻿using FractionGreaterThanOne;
+
+var p1 = new Fraction();
+Console.WriteLine("Is greater than one: " + p1.IsGreaterThanOne("1/2"));
+Console.WriteLine("Is greater than one: " + p1.IsGreaterThanOne("7/4"));
+Console.WriteLine("Is greater than one: " + p1.IsGreaterThanOne("10/10"));
