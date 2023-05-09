baf0d6df0941bd481a183350edf9cb5b8b444181
Wizmi24 <116069858+Wizmi24@users.noreply.github.com>
2023-05-09 17:43:22 +0200

    Add files via upload

---
+++ TraceListener/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+
+Console.WriteLine($"Writing to: {logPath}");
+
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+
+Trace.Listeners.Add(logFile);
+
+Trace.AutoFlush = true;
+
+Debug.WriteLine("Debug says, I am wayching");
+Trace.WriteLine("Trace says, I am watching");
+
+Console.WriteLine($"Reading from appsettings.json in {Directory.GetCurrentDirectory()}");
+
+ConfigurationBuilder builder = new();
+
+builder.SetBasePath(Directory.GetCurrentDirectory());
+
+builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+
+TraceSwitch ts = new(
+    displayName: "PacktSwitch",
+    description: "This switch is set via a JSON config."
+    );
+
+configuration.GetSection("PacktSwitch").Bind(ts);
+
+Trace.WriteLineIf(ts.TraceError, "Trace error");
+Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+
+Console.ReadLine();
